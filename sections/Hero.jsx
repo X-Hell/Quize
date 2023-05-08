68c8e4cfd3d677270cd69830f345b20775bdf2d5
Manish Shaw <monishofficial8@gmail.com>
2023-05-08 06:03:44 +0530

    Debugging

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,18 +1,17 @@
-'use client';
+"use client";
 
-import {motion} from 'framer-motion'
-import styles from '../styles'
-import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
-    <motion.div 
+    <motion.div
       variants={staggerContainer}
-      initial='hidden'
-      whileInView='show'
+      initial="hidden"
+      whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-    >
-    </motion.div>
+    ></motion.div>
   </section>
 );
 
